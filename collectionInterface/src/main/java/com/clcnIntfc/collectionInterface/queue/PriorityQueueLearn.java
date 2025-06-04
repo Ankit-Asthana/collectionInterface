07ec4856d86f7c691a51c2877525ad8561d4bc35
AnkitMjolnir <ankit@global.com>
2025-06-04 23:39:27 +0530

    Added Queue and PriorityQueue

---
+++ collectionInterface/src/main/java/com/clcnIntfc/collectionInterface/queue/PriorityQueueLearn.java
@@ -0,0 +1,116 @@
+package com.clcnIntfc.collectionInterface.queue;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import javax.print.attribute.standard.MediaSize;
+import javax.swing.*;
+import java.util.PriorityQueue;
+
+public class PriorityQueueLearn {
+    public static void main(String[] args) {
+        /*
+            Part of Queue interface
+            Order elements based on their natural ordering (for primitive data type ->  Lowest first)
+            For class, custom Comparator or Class with implemented Comparable for ordering
+            Doesn't allow null element
+
+            Internal working of PriorityQueue
+                Implements as a Min-heap by default
+                    Min-heap work: The both child elements must be greater than the parent.
+                    
+                When polled/removed the head, then only the next high priority element will come on head
+         */
+
+        PriorityQueue<Integer> integers = new PriorityQueue<>();
+        integers.offer(15);
+        integers.offer(3);
+        integers.offer(2);
+        integers.offer(-1);
+        System.out.println("Integer priority queue size after adding all is "+ integers.size());
+
+        while(!integers.isEmpty()) {              // remove the head and prints
+            System.out.println(integers.poll()); //Prints the sorted order [-1, 2, 3, 15] , coz of the lowest first priority insertion
+        }
+        System.out.println("Integer priority queue size after polling " +integers.size());
+        System.out.println();
+
+        /*
+        Using the Comparable for priority based insertion in PriorityQueue
+         */
+        PriorityQueue<People> peoplePriorityQueue = new PriorityQueue<>();
+        peoplePriorityQueue.offer(new People(2, "Ankit"));
+        peoplePriorityQueue.offer(new People(1, "Aryan"));
+        peoplePriorityQueue.offer(new People(7, "Omkar"));
+        System.out.println("peoplePriorityQueue size after adding all is" +integers.size());
+
+
+
+        while(!peoplePriorityQueue.isEmpty()) {
+            System.out.println(peoplePriorityQueue.poll());  //remove the head and prints, so we can know that what is the order in which the elements were placed
+        }
+        System.out.println("peoplePriorityQueue size after polling " +peoplePriorityQueue.size());
+        System.out.println();
+
+
+        /*
+        Using the comparator for priority based insertion in Priority Queue
+         */
+
+        PriorityQueue<Engineer> engineerPriorityQueue = new PriorityQueue<>((a, b) -> b.getEngid() - a.getEngid());  //Desc. order comparator
+        engineerPriorityQueue.offer(new Engineer(1, "Employee"));
+        engineerPriorityQueue.offer(new Engineer(2,"Lead" ));
+        engineerPriorityQueue.offer(new Engineer(3, "TechLead"));
+        engineerPriorityQueue.offer(new Engineer(4, "Project Manager"));
+        engineerPriorityQueue.offer(new Engineer(5, "Manager"));
+        engineerPriorityQueue.offer(new Engineer(6, "CTO"));
+        engineerPriorityQueue.offer(new Engineer(7, "COO"));
+        engineerPriorityQueue.offer(new Engineer(8, "CEO"));
+
+        System.out.println("engineerPriorityQueue size after adding all " +engineerPriorityQueue.size());
+
+        while (!engineerPriorityQueue.isEmpty()) {
+            System.out.println(engineerPriorityQueue.poll());
+        }
+        System.out.println("engineerPriorityQueue size after polling " +engineerPriorityQueue.size());
+
+    }
+}
+
+
+@Data
+class People implements Comparable<People> {
+
+    private int id;
+    private String name;
+
+    @Override
+    public int compareTo(People o) {
+        return (o.getId() - this.getId()); //descending order
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public People(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
+@Data
+@AllArgsConstructor
+@ToString
+class Engineer {
+    private int engid;
+    private String degn;
+
+}
--- collectionInterface/src/main/java/com/clcnIntfc/collectionInterface/queue/QueueLearn.java
+++ collectionInterface/src/main/java/com/clcnIntfc/collectionInterface/queue/QueueLearn.java
@@ -1,4 +1,46 @@
 package com.clcnIntfc.collectionInterface.queue;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class QueueLearn {
+    /*
+    Queue is an interface in Java so we can't instanciate it. We should instanciate it's implementation class.
+    Implementation classes
+        1. LinkedList
+        2. PriorityQueue
+        3. BlockingQueue
+        4. ArrayBlockingQueue
+
+    A data structure that runs on First in First out principle
+    Basic Functionality
+        Enqueue - Add element at end                  fn ->  (add(), offer())
+        Dequeue - Remove element from head            fn ->  (remove(), poll())
+        Peek - To see the element at head             fn ->  (peek(), element())
+     */
+
+    public static void main(String[] args) {
+        Queue<Integer> integerQueue = new LinkedList<>();
+
+        //For adding element in Queue
+//        System.out.println(integerQueue.remove());         //throw the exception, if queue is Empty
+        System.out.println(integerQueue.poll());           //returns null if Queue is empty
+
+        //For removing finding the element at head of queue
+//        System.out.println(integerQueue.element());      //throw exception, if Queue is empty
+        System.out.println(integerQueue.peek());         // returns null if Queue is empty
+
+
+        Queue<Integer> blockingQueue = new ArrayBlockingQueue<>(2);  //fixed the capacity 2
+        blockingQueue.add(1);
+        blockingQueue.offer(2);
+
+//        blockingQueue.add(3);               //throws exception if queue is full
+        blockingQueue.offer(3);
+
+    }
+
 }
